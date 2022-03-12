83d73ca6a2865bbf79c98ef825ec3d8ef44be93c
Andreas Celada <andreas.sch.celada@gmail.com>
2022-03-12 15:25:09 -0300

    Navigation Buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from 'react-router-dom';
 
 function Listing() {
     const movie = {
@@ -35,7 +36,11 @@ function Listing() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to = '/'>
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+
+                </Link>
+
             </div >
         </div >
     );
