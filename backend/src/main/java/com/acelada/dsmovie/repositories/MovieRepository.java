c17de988367201e70b505759d199d7446f3d17f7
Andreas Celada <andreas.sch.celada@gmail.com>
2022-03-13 15:17:16 -0300

    Find Movies

---
+++ backend/src/main/java/com/acelada/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.acelada.dsmovie.repositories;
+
+import com.acelada.dsmovie.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+}
